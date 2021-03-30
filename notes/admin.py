5c92e91d0bbe9effdc8354aded2b67462e96ce80
Amy Gori <amydgori@gmail.com>
2021-03-30 18:52:58 -0400

    Add Tag model and relationship to Note

--- notes/admin.py
+++ notes/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import Note, User
+from .models import Note, User, Tag
 
 admin.site.register(Note)
+admin.site.register(Tag)
 admin.site.register(User, UserAdmin)
--- notes/models.py
+++ notes/models.py
@@ -6,11 +6,19 @@ class User(AbstractUser):
     pass
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Note(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
     user = models.ForeignKey(
         to=User, on_delete=models.CASCADE, related_name="notes")
+    tags = models.ManyToManyField(to=Tag, related_name="notes", blank=True)
 
     def __str__(self):
         return self.title
