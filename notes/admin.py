044c5722cf6c0ecc65382af23e44c6c2d6285824
Amy Gori <amydgori@gmail.com>
2021-03-30 15:45:56 -0400

    Add User to admin

--- notes/admin.py
+++ notes/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Note
+from django.contrib.auth.admin import UserAdmin
+
+from .models import Note, User
 
 admin.site.register(Note)
+admin.site.register(User, UserAdmin)
