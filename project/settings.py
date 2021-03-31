7262505b44659e3ae77e21110d1dbf6d7b2b1a0c
Amy Gori <amydgori@gmail.com>
2021-03-31 16:44:14 -0400

    Configure for heroku

--- project/settings.py
+++ project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+import django_on_heroku
 import os
 from pathlib import Path
 
@@ -152,3 +153,6 @@ REST_FRAMEWORK = {
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+django_on_heroku.settings(locals())
+del DATABASES['default']['OPTIONS']['sslmode']
